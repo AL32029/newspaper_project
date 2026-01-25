0301d215a40e6f92fc7efba90c288124d3800bc7
Nikita Bondarev <nikitabondarevvitebsk@yandex.by>
2026-01-25 17:00:04 +0300

    - Правки до итогового задания 25.7

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -8,6 +8,11 @@ class NewsList(ListView):
     ordering = '-created_at'
     template_name = 'news_list.html'
     context_object_name = 'news'
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        return context
 
 
 class NewsInfo(DetailView):
