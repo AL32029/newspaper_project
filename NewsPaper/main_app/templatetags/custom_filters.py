027b715e25aaf2a9fef2ef53e2aa56ae6f955c5d
Nikita Bondarev <nikitabondarevvitebsk@yandex.by>
2026-02-01 20:14:08 +0300

    Оформлена главная страница

---
+++ NewsPaper/main_app/templatetags/custom_filters.py
@@ -0,0 +1,28 @@
+import datetime
+
+from django import template
+
+register = template.Library()
+forbidden_words = [
+    "автосалоне",
+    "представлена",
+    "нового",
+    "бренд",
+    "технологии"
+]
+
+
+@register.filter
+def censor(value):
+    value_split = value.split()
+
+    for index, value_str in enumerate(value_split):
+        if value_str.lower() in forbidden_words:
+            value_split[index] = value_str[0] + "*" * (len(value_str) - 1)
+
+    return " ".join(value_split)
+
+
+@register.filter
+def format_date(datetime_str, format_str="%d.%m.%Y %H:%M:%S"):
+    return datetime.datetime.strftime(datetime_str, format_str)
--- NewsPaper/main_app/views.py
+++ NewsPaper/main_app/views.py
@@ -1,5 +1,10 @@
 from django.http.request import HttpRequest
 from django.shortcuts import render
 
+from news.models import Post
+
+
 def index_view(request: HttpRequest):
-    return render(request, 'main_templates/index.html')
+    return render(request, 'main_templates/index.html', {
+        "posts": Post.objects.order_by("-created_at")[:10]
+    })
