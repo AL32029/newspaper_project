bd69e81c573341ab391b5ab8df438d4ba2e02661
Nikita Bondarev <nikitabondarevvitebsk@yandex.by>
2026-02-07 21:59:33 +0300

    Интегрировано кеширование файлов

--- NewsPaper/main_app/views.py
+++ NewsPaper/main_app/views.py
@@ -1,9 +1,10 @@
 from django.http.request import HttpRequest
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 
 from news.models import Post
 
-
+@cache_page(60)
 def index_view(request: HttpRequest):
     return render(request, 'main_templates/index.html', {
         "posts": Post.objects.order_by("-created_at")[:10]
