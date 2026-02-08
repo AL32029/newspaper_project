af315e8c881a49f96fbed32b84ff7f763ca4370a
Nikita Bondarev <nikitabondarevvitebsk@yandex.by>
2026-02-08 18:51:43 +0300

    Перенос базы данных с sqlite на postgresql

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -104,8 +104,12 @@ WSGI_APPLICATION = 'NewsPaper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'newspaper',
+        'USER': 'newspaper',
+        'PASSWORD': '***',
+        'HOST': '****',
+        'PORT': '5432',
     }
 }
 
