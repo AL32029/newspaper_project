81c295a31081f6ab272f0e92bb067440e51383b6
Nikita Bondarev <nikitabondarevvitebsk@yandex.by>
2026-02-08 16:49:59 +0300

    Добавлены объекты постов и категорий в админ-панели

--- NewsPaper/news/admin.py
+++ NewsPaper/news/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Category, Post
 
-# Register your models here.
+
+class PostsAdmin(admin.ModelAdmin):
+    list_display = [
+        field.name for field in Post._meta.get_fields()
+        if field.__class__.__name__ not in [
+            'ManyToManyField',
+            'ForeignKey',
+            'OneToOneField',
+            'ManyToOneRel',
+            'ManyToManyRel'
+        ]
+           and not field.is_relation
+    ]
+
+    list_filter = ('post_type', 'created_at',)
+    search_fields = ('title',)
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = [
+        field.name for field in Category._meta.get_fields()
+        if field.__class__.__name__ not in [
+            'ManyToManyField',
+            'ForeignKey',
+            'OneToOneField',
+            'ManyToOneRel',
+            'ManyToManyRel'
+        ]
+           and not field.is_relation
+    ]
+
+    list_filter = ('name', )
+
+
+admin.site.register(Category, CategoriesAdmin)
+admin.site.register(Post, PostsAdmin)
