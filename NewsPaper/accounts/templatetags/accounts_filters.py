a5f3c56688b4e29c9cd4668b6436186343dbeaf1
Nikita Bondarev <nikitabondarevvitebsk@yandex.by>
2026-02-08 18:50:36 +0300

    Внесены мелкие изменения

--- NewsPaper/accounts/templatetags/accounts_filters.py
+++ NewsPaper/accounts/templatetags/accounts_filters.py
@@ -3,6 +3,27 @@ import datetime
 from django import template
 
 register = template.Library()
+forbidden_words = [
+    "автосалоне",
+    "представлена",
+    "нового",
+    "бренд",
+    "технологии"
+]
+
+
+@register.filter
+def censor(value):
+    """
+    Цензурирование заголовка и текста постов
+    """
+    value_split = value.split()
+
+    for index, value_str in enumerate(value_split):
+        if value_str.lower() in forbidden_words:
+            value_split[index] = value_str[0] + "*" * (len(value_str) - 1)
+
+    return " ".join(value_split)
 
 
 @register.filter
